882e6a22ade6aba2c55a03ff463190eadca875aa
Tanvir Ahmed <tanvir.rafee405@gmail.com>
2021-10-24 15:45:00 +0600

    config.php updated

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "myfeathe_portaladmin";
-$password = "myfeatheportaladmin";
+$username = "root";
+$password = "";
 $dbname="myfeathe_portal";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
